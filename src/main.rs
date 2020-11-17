7eef5c0faf2c366c0d841c67372195af79da6d39
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-11-17 03:14:03 -0300

    ✨ parsing port argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Jaguar")
+        .version("0.1")
+        .author("Leonardo Lima <leonardoaugusto287@gmail.com>")
+        .about("Test socket connetions")
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Port to be interacted with")
+            .takes_value(true))
+        .get_matches();
+
+    let port = matches.value_of("port");
+    println!("PORT: {:?}", port);
 }
